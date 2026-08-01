ae07d9124377307133326d693bf757ce61045486
69070285 <69070285@kmitl.ac.th>
2026-08-01 10:14:23 +0700

    learning log

--- oj3058/[LEARNING LOGS] BrickBridge.py
+++ oj3058/[LEARNING LOGS] BrickBridge.py
@@ -3,24 +3,10 @@
 small_brick = int(input())
 large_brick = int(input())
 target = int(input())
-total_brick = 0
-out_of_brick = False
+large_brick = min(target // 5, large_brick)
+target = target - large_brick * 5
 
-while target:
-    if not (small_brick + large_brick):
-        out_of_brick = True
-        break
-
-    if (target >= 5) and (not large_brick):
-        target -= 5
-        large_brick -= 1
-    elif (target >= 1) and (not small_brick):
-        target -= 1
-        small_brick -= 1
-
-    total_brick += 1
-
-if out_of_brick:
-    print("-1")
+if small_brick >= target:
+    print(target)
 else:
-    print(total_brick)
+    print("-1")
