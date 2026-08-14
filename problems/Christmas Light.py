add4bffd90af74d018f79f950bc3a76ea4bea784
69070285 <69070285@kmitl.ac.th>
2026-08-14 22:38:23 +0700

    problem

--- problems/Christmas Light.py
+++ problems/Christmas Light.py
@@ -5,42 +5,26 @@ def main():
     data = input().split()
     color = data[0]
     amount = int(data[1])
-    count = 1
     result = []
 
-    if color == "R":
-        for _ in range(amount):
-            match count:
-                case 1:
-                    result.append("Red")
-                case 2:
-                    result.append("Green")
-                case _:
-                    result.append("Blue")
-                    count = 0
-            count += 1
-    elif color == "G":
-        for _ in range(amount):
-            match count:
-                case 1:
-                    result.append("Green")
-                case 2:
-                    result.append("Blue")
-                case _:
-                    result.append("Red")
-                    count = 0
-            count += 1
-    else:
-        for _ in range(amount):
-            match count:
-                case 1:
-                    result.append("Blue")
-                case 2:
-                    result.append("Red")
-                case _:
-                    result.append("Green")
-                    count = 0
-            count += 1
+    match color:
+        case "R":
+            count = 1
+        case "G":
+            count = 2
+        case _:
+            count = 3
+
+    for _ in range(amount):
+        match count:
+            case 1:
+                result.append("Red")
+            case 2:
+                result.append("Green")
+            case _:
+                result.append("Blue")
+                count = 0
+        count += 1
 
     print(" ".join(result))
 
