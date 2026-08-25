6f72a1f46dae563982c70450e8ec7a83c0ddd27c
69070285 <69070285@kmitl.ac.th>
2026-08-25 07:26:54 +0000

    problem

--- problems/Bonus.py
+++ problems/Bonus.py
@@ -6,23 +6,23 @@ exp = int(data[1])
 earn = int(data[2])
 
 if role == "M":
-    if exp < 5:
+    if exp <= 5:
         print(int(earn * 0.06 + 1500))
-    elif 5 <= exp <= 10:
+    elif 5 < exp <= 10:
         print(int(earn * 0.08 + 1500))
     else:
         print(int(earn * 0.1 + 1500))
 elif role == "B":
-    if exp < 5:
+    if exp <= 5:
         print(int(earn * 0.05 + 1000))
-    elif 5 <= exp <= 10:
+    elif 5 < exp <= 10:
         print(int(earn * 0.06 + 1000))
     else:
         print(int(earn * 0.07 + 1000))
 else:
-    if exp < 5:
+    if exp <= 5:
         print(int(earn * 0.04 + 500))
-    elif 5 <= exp <= 10:
+    elif 5 < exp <= 10:
         print(int(earn * 0.05 + 500))
     else:
         print(int(earn * 0.06 + 500))
